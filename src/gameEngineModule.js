c6145a43b09a8e44e493aa61db36b1b0d39a8e1e
jwarmkessel <justin@rewardme.com>
2013-07-08 17:09:45 -0700

    Select winner function now does batch notifications but doesn't distinguish between winners and non-winners.

--- src/gameEngineModule.js
+++ src/gameEngineModule.js
@@ -1,5 +1,16 @@
 var gameEngineModuleHandler = function(app) {
   console.log("including gameEngineModule");
+  
+  var express = require('express')
+      ,fs = require('fs')
+      ,crypto = require('crypto')
+      ,tls = require('tls')
+      ,certPem = fs.readFileSync('GoosiiCert.pem', encoding='ascii')
+      ,keyPem = fs.readFileSync('GoosiiKey-noenc.pem', encoding='ascii')
+      ,caCert = fs.readFileSync('aps_development.cer', encoding='ascii')
+      ,options = { key: keyPem, cert: certPem, ca: [ caCert ] }
+      ,http = require('http');
+    
   var check = require('validator').check
     ,sanitize = require('validator').sanitize
     
@@ -49,15 +60,86 @@ var gameEngineModuleHandler = function(app) {
     });
   });
 
+  //Used for creating connection APNS server    	
+	function hextobin(hexstr) {
+      buf = new Buffer(hexstr.length / 2);
+
+      for(var i = 0; i < hexstr.length/2 ; i++)
+      {
+          buf[i] = (parseInt(hexstr[i * 2], 16) << 4) + (parseInt(hexstr[i * 2 + 1], 16));
+      }
+
+      return buf;
+  }
+  
+  //Send notification test to Justin's phone
+  var sendPushNotification = function(pushIdentifier, pushMessage, customParameter){
+
+    next = function(){};
+
+    var stream = tls.connect(2195, 'gateway.sandbox.push.apple.com', options, function() {
+        console.log("Connecting with APNS");
+        // connected
+        next( !stream.authorized, stream );
+        console.log("Connected with APNS" + stream.authorized);
+    });
+
+    var
+        pushnd = { aps: { alert: pushMessage }, customParam: { foo: 'bar' } } // 'aps' is required
+        ,hextoken = pushIdentifier // Push token from iPhone app. 32 bytes as hexadecimal string
+        ,token = hextobin(hextoken)
+        ,payload = JSON.stringify(pushnd)
+        ,payloadlen = Buffer.byteLength(payload, 'utf-8')
+        ,tokenlen = 32
+        ,buffer = new Buffer(1 +  4 + 4 + 2 + tokenlen + 2 + payloadlen)
+        ,i = 0
+        ,msgid = 0xbeefcace // message identifier, can be left 0
+        ,seconds = Math.round(new Date().getTime() / 1000) + 1*60*60 // expiry in epoch seconds (1 hour)
+        ,payload = JSON.stringify(pushnd);
+    ;
+
+    buffer[i++] = 1; // command
+    buffer[i++] = msgid >> 24 & 0xFF;
+    buffer[i++] = msgid >> 16 & 0xFF;
+    buffer[i++] = msgid >> 8 & 0xFF;
+    buffer[i++] = msgid & 0xFF;
+
+    // expiry in epoch seconds (1 hour)
+    buffer[i++] = seconds >> 24 & 0xFF;
+    buffer[i++] = seconds >> 16 & 0xFF;
+    buffer[i++] = seconds >> 8 & 0xFF;
+    buffer[i++] = seconds & 0xFF;
+
+    buffer[i++] = tokenlen >> 8 & 0xFF; // token length
+    buffer[i++] = tokenlen & 0xFF;
+    token = hextobin(hextoken);
+    token.copy(buffer, i, 0, tokenlen)
+    i += tokenlen;
+    buffer[i++] = payloadlen >> 8 & 0xFF; // payload length
+    buffer[i++] = payloadlen & 0xFF;
+
+    payload = Buffer(payload);
+    payload.copy(buffer, i, 0, payloadlen);
+    stream.write(buffer);  // write push notification
+
+    stream.on('data', function(data) {
+
+        var
+            command = data[0] & 0x0FF  // always 8
+            ,status = data[1] & 0x0FF  // error code
+            ,msgid = (data[2] << 24) + (data[3] << 16) + (data[4] << 8 ) + (data[5])
+        ;
+
+        console.log(command + ':' + status + ':' + msgid);
+    });
+  };
   
   var selectWinner = function(participants) {
-    for (var key in result)
+    for (var key in participants)
     {
-       if(result.hasOwnProperty(key)) { 
-         console.log(result[key].userId + result[key].pushIdentifier);
-         
-         //Out of all the participants randomly select who the winner is and send a special notification to the winner.
-         //Add non-winners to an array that will end up being the batch of notifications that will be notified
+       if(participants.hasOwnProperty(key)) {      
+         console.log("About to send notify to " + participants[key].pushIdentifier)
+         sendPushNotification(participants[key].pushIdentifier, 'You are user' + participants[key].userId);
        }
     }
   }
@@ -75,6 +157,7 @@ var gameEngineModuleHandler = function(app) {
         if (err) console.warn(err.message);
         
         if(object) {
+          console.log("calling selectWinner()");
           selectWinner(object.participants);
 
           res.send("determining winner");
