262fd2920ce7144d856a28565a208ce280d0bfb4
jwarmkessel <justin@rewardme.com>
2013-07-20 01:00:53 -0700

    Cleaned up code in users api file. Opitimized mongodb queries, added clear commments, and deleted unnecessary code.

--- src/usersModule.js
+++ src/usersModule.js
@@ -52,17 +52,17 @@ var usersModuleHandler = function(app) {
     });
   });
 
-  //loginUser requires the additional pushIdentifier because these tokens may change. 
+  //loginUser requires the additional pushIdentifier because these tokens may change.
+  //Also updates the lastlogin.
   app.get('/loginUser/:userId/:pushIdentifier', function(req, res) {
     var utc_timestamp = utilitiesModule.getCurrentUtcTimestamp();
 
     db.open(function (error, client) {
       if (error) throw error;    
-      var collection = new mongodb.Collection(client, 'users');
-
+      //validate the id string.
       try {
         var checker = check(req.params.userId).len(24).isHexadecimal();   
-        console.log("The checker : " + checker);
+        console.log("The id is : " + checker);
       }catch (e) {
         console.log(e.message); //Please enter a valid integer
         res.send("There was a problem with the userID");
@@ -70,36 +70,31 @@ var usersModuleHandler = function(app) {
 
         return;
       }
-
-      collection.findOne({_id: new ObjectID(req.params.userId)}, function(error, object) {
-        if(object) {
-          //Example of multiple set updates: 
-          /*
-          collection.update({_id: ObjectID(req.params.userId)}, {$set: {'user.lastlogin':utc_timestamp, 'user.lastName':''}}, {safe:true}, function(err, result) {
-          */        
-          
-          userObject = {"lastlogin" : utc_timestamp, "pushIdentifier" : req.params.pushIdentifier };
-
-          console.log("The last time I logged in was " + utc_timestamp);
-          collection.update({_id: ObjectID(req.params.userId)}, {$set: userObject}, {safe:true}, function(err, result) {
-
-            if (err) console.warn(err.message);
-            if (err && err.message.indexOf('E11000 ') !== -1) {
-              // this _id was already inserted in the database
-              console.log("E11000 error");
-            }
-          });
+      
+      //Retrieve the users collection.
+      var usersMongo = new mongodb.Collection(client, 'users');
+      
+      //Create the JSON object filled with elements to update.
+      userObject = {"lastlogin" : utc_timestamp, "pushIdentifier" : req.params.pushIdentifier };      
+      
+      //Query and update the parameters for this userId.
+      usersMongo.update({_id: new ObjectID(req.params.userId)}, {$set: userObject}, function(err, object) {
+        if(!err && object) {
           res.send(object);
-          db.close();                
+        } else {
+          console.warn(err.message);
         }
+        db.close();
       });
     });
   });
 
+  //Query and return the company objects for all the events the user is participating in. 
   app.get('/getUserContests/:userId', function(req, res) {
 
     //The array of company id's to query in companies.
     companyObjArray = new Array();
+    
     db.open(function (error, client) {    
       var usersMongo = new mongodb.Collection(client, 'users');
       var companiesMongo = new mongodb.Collection(client, 'companies');      
@@ -112,6 +107,7 @@ var usersModuleHandler = function(app) {
             companyObjArray.push(companyObjectId);
           }
           
+          //Query the companies collection and return the list of company objects.
           companiesMongo.findOne({"_id": {$in : companyObjArray }}, function(err, companyObj) {
             if(!err) {
               //Now that I have the list of company objects I can pass it back and modify the order here 
@@ -123,34 +119,7 @@ var usersModuleHandler = function(app) {
       });
     });
   });
-//This addCheckin is no longer necessary.  
-  // app.get('/addCheckin/:userId/:companyId', function(req, res) {
-  //   var utc_timestamp = utilitiesModule.getCurrentUtcTimestamp();
-  // 
-  //   //Open the database
-  //   db.open(function (error, client) {
-  //     if (error) throw error;
-  // 
-  //     var userCheckinObj = {
-  //                        "companyId" : req.params.companyId,
-  //                        "timestamp" : utc_timestamp
-  //                      }
-  // 
-  //     var users = new mongodb.Collection(client, 'users');
-  //     users.update({_id: ObjectID(req.params.userId)}, {$push: { "user.checkins": userCheckinObj} }, {safe:true}, function(err, result) {
-  //       if (err) console.warn(err.message);
-  //       if (err && err.message.indexOf('E11000 ') !== -1) {
-  //         // this _id was already inserted in the database
-  //         console.log("E11000 error");
-  //       }
-  // 
-  //       //send the response to the client
-  //       res.send(objects);
-  //       db.close()
-  //     });    
-  //   });
-  // });
-  
+
 };
 
 exports.usersModuleHandler = usersModuleHandler;
